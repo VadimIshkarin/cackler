2f76f56e2f2599e98be3b0c45a8ae732c11fd5d0
VadimIshkarin <theishkarin@gmail.com>
2022-10-25 16:39:21 -0400

    react-native-reanimated/plugin

--- components/Chat.js
+++ components/Chat.js
@@ -2,10 +2,11 @@ import React from "react";
 import { View, Text, Button } from "react-native";
 
 export default class Chat extends React.Component {
-  render() {
+  componentDidMount() {
     let name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name });
-
+  }
+  render() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Button
