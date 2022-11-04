1b2755ea6ea142ea2ef4e1142a5ae59dd0b04082
VadimIshkarin <theishkarin@gmail.com>
2022-11-04 10:58:23 -0400

    Added changes to files

--- components/Chat.js
+++ components/Chat.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { GiftedChat, Bubble, InputToolbar } from "react-native-gifted-chat";
 import { StyleSheet, View, Platform, KeyboardAvoidingView } from "react-native";
-//Data from myFirestore database
-const firebase = require("firebase");
-require("firebase/firestore");
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import NetInfo from "@react-native-community/netinfo";
-import * as Permissions from "expo-permissions";
-import * as ImagePicker from "expo-image-picker";
-import * as Location from "expo-location";
 import MapView from "react-native-maps";
 import CustomActions from "./CustomActions";
+
+const firebase = require("firebase");
+require("firebase/firestore");
+
 export default class Chat extends React.Component {
   constructor() {
     super();
@@ -102,6 +100,8 @@ export default class Chat extends React.Component {
           _id: data.user._id,
           name: data.user.name,
         },
+        image: data.image || null,
+        location: data.location || null,
       });
     });
     this.setState({
@@ -173,6 +173,8 @@ export default class Chat extends React.Component {
       text: message.text,
       createdAt: message.createdAt,
       user: message.user,
+      image: message.image,
+      location: message.location,
     });
   };
 
@@ -239,6 +241,8 @@ export default class Chat extends React.Component {
             renderCustomView={this.renderCustomView}
             user={{
               _id: this.state.user._id,
+              name: this.state.name,
+              avatar: this.state.user.avatar,
             }}
           />
           {Platform.OS === "android" ? (
@@ -260,166 +264,6 @@ const styles = StyleSheet.create({
     width: "88%",
     paddingBottom: 10,
     justifyContent: "center",
+    borderRadius: 5,
   },
 });
-
-//WITHOUT FIRESTORE
-
-//   componentDidMount() {
-//     let name = this.props.route.params.name;
-//     this.props.navigation.setOptions({ title: name });
-
-//     this.setState({
-//       messages: [
-//         {
-//           _id: 1,
-//           text: "Hey Vadim! What's up?",
-//           createdAt: new Date(),
-//           user: {
-//             _id: 2,
-//             name: "React Native",
-//             avatar: "https://placeimg.com/140/140/any",
-//           },
-//         },
-//         {
-//           _id: 2,
-//           text: `${name} has entered the ChitChat`,
-//           createdAt: new Date(),
-//           system: true,
-//         },
-//       ],
-//     });
-//   }
-//   onSend(messages = []) {
-//     this.setState((previousState) => ({
-//       messages: GiftedChat.append(previousState.messages, messages),
-//     }));
-//   }
-//   renderBubble(props) {
-//     return (
-//       <Bubble
-//         {...props}
-//         wrapperStyle={{
-//           left: {
-//             backgroundColor: "whitesmoke",
-//           },
-//           right: {
-//             backgroundColor: "#05676e",
-//           },
-//         }}
-//       />
-//     );
-//   }
-
-//   render() {
-//     // Set the background color selected from start screen
-//     const { color } = this.props.route.params;
-//     return (
-//       <View style={[{ backgroundColor: color }, styles.container]}>
-//         <View style={styles.giftedChat}>
-//           <GiftedChat
-//             renderBubble={this.renderBubble.bind(this)}
-//             messages={this.state.messages}
-//             onSend={(messages) => this.onSend(messages)}
-//             user={{
-//               _id: 1,
-//             }}
-//           />
-//           {Platform.OS === "android" ? (
-//             <KeyboardAvoidingView behavior="height" />
-//           ) : null}
-//         </View>
-//       </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-//   giftedChat: {
-//     flex: 1,
-//     width: "88%",
-//     paddingBottom: 10,
-//     justifyContent: "center",
-//   },
-// });
-
-//Another way using function:
-// import React, { useState, useEffect, useCallback } from "react";
-// import { GiftedChat, Bubble } from "react-native-gifted-chat";
-// import { StyleSheet, View, Platform, KeyboardAvoidingView } from "react-native";
-
-// export default function Chat(props) {
-//   let { name, color } = props.route.params;
-//   const [messages, setMessages] = useState([]);
-
-//   // Set the screen title to the user name entered in the start screen
-//   useEffect(() => {
-//     props.navigation.setOptions({ title: name });
-//     setMessages([
-//       {
-//         _id: 1,
-//         text: "Hello Vadim",
-//         createdAt: new Date(),
-//         user: {
-//           _id: 2,
-//           name: "React Native",
-//           avatar: "https://placeimg.com/140/140/any",
-//         },
-//       },
-//       {
-//         _id: 2,
-//         text: `${name}has entered the chat`,
-//         createdAt: new Date(),
-//         system: true,
-//         // Any additional custom parameters are passed through
-//       },
-//     ]);
-//   }, []);
-
-//   const onSend = useCallback((messages = []) => {
-//     setMessages((previousMessages) =>
-//       GiftedChat.append(previousMessages, messages)
-//     );
-//   }, []);
-
-//   // Customize the color of the sender bubble
-//   const renderBubble = (props) => {
-//     return (
-//       <Bubble
-//         {...props}
-//         wrapperStyle={{
-//           right: {
-//             backgroundColor: "#000",
-//           },
-//         }}
-//       />
-//     );
-//   };
-
-//   return (
-//     <View style={[{ backgroundColor: color }, styles.container]}>
-//       <GiftedChat
-//         renderBubble={renderBubble.bind()}
-//         messages={messages}
-//         onSend={(messages) => onSend(messages)}
-//         user={{
-//           _id: 1,
-//         }}
-//       />
-//       {Platform.OS === "android" ? (
-//         <KeyboardAvoidingView behavior="height" />
-//       ) : null}
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   },
-// });
---
+++ components/CustomActions.js
@@ -0,0 +1,179 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { TouchableOpacity, View, Text, StyleSheet } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as Location from "expo-location";
+
+import { connectActionSheet } from "@expo/react-native-action-sheet";
+import firebase from "firebase";
+import "firebase/firestore";
+
+export default class CustomActions extends React.Component {
+  // you can choose an image from your phone to send
+  pickImage = async () => {
+    // permission to access the user phone pics
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    try {
+      if (status === "granted") {
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        }).catch((error) => {
+          console.log(error);
+        });
+        if (!result.cancelled) {
+          const imageUrl = await this.uploadImage(result.uri);
+          this.props.onSend({ image: imageUrl });
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  // Take a photo with your phone's camera
+  takePhoto = async () => {
+    // permission to access user camera
+    const { status } = await ImagePicker.requestCameraPermissionsAsync();
+    try {
+      if (status === "granted") {
+        let result = await ImagePicker.launchCameraAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        }).catch((error) => {
+          console.log(error);
+        });
+        if (!result.cancelled) {
+          const imageUrl = await this.uploadImage(result.uri);
+          this.props.onSend({ image: imageUrl });
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  // converting your image files into blobs, uploading to Firestore cloud storage
+  uploadImage = async (uri) => {
+    const blob = await new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+        resolve(xhr.response);
+      };
+      xhr.onerror = function (e) {
+        console.log(e);
+        reject(new TypeError("Network request failed"));
+      };
+      xhr.responseType = "blob";
+      xhr.open("GET", uri, true);
+      xhr.send(null);
+    });
+    // create storage reference
+    const imageNameBefore = uri.split("/");
+    const imageName = imageNameBefore[imageNameBefore.length - 1];
+
+    const ref = firebase.storage().ref().child(`images/${imageName}`);
+
+    const snapshot = await ref.put(blob);
+
+    blob.close();
+
+    return await snapshot.ref.getDownloadURL();
+  };
+
+  // Access and send the user's location
+  getLocation = async () => {
+    // permission to access user location while the app is in the foreground
+    const { status } = await Location.requestForegroundPermissionsAsync();
+    try {
+      if (status === "granted") {
+        let result = await Location.getCurrentPositionAsync({}).catch(
+          (error) => {
+            console.log(error);
+          }
+        );
+        // Send latitude and longitude to locate the position on the map
+        if (result) {
+          this.props.onSend({
+            location: {
+              longitude: result.coords.longitude,
+              latitude: result.coords.latitude,
+            },
+          });
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  onActionPress = () => {
+    const options = [
+      "Choose From Library",
+      "Take Picture",
+      "Send Location",
+      "Cancel",
+    ];
+    const cancelButtonIndex = options.length - 1;
+    this.props.showActionSheetWithOptions(
+      {
+        options,
+        cancelButtonIndex,
+      },
+      async (buttonIndex) => {
+        switch (buttonIndex) {
+          case 0:
+            console.log("user wants to pick an image");
+            return this.pickImage();
+          case 1:
+            console.log("user wants to take a photo");
+            return this.takePhoto();
+          case 2:
+            console.log("user wants to get their location");
+            return this.getLocation();
+        }
+      }
+    );
+  };
+
+  render() {
+    return (
+      <TouchableOpacity
+        accessible={true}
+        accessibilityLabel="More options"
+        accessibilityHint="Choose to send an image or your geolocation."
+        style={[styles.container]}
+        onPress={this.onActionPress}
+      >
+        <View style={[styles.wrapper, this.props.wrapperStyle]}>
+          <Text style={[styles.iconText, this.props.iconTextStyle]}>+</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 26,
+    height: 26,
+    marginLeft: 10,
+    marginBottom: 10,
+  },
+  wrapper: {
+    borderRadius: 13,
+    borderColor: "#b2b2b2",
+    borderWidth: 2,
+    flex: 1,
+  },
+  iconText: {
+    color: "#b2b2b2",
+    fontWeight: "bold",
+    fontSize: 16,
+    backgroundColor: "transparent",
+    textAlign: "center",
+  },
+});
+
+CustomActions.contextTypes = {
+  actionSheet: PropTypes.func,
+};
+CustomActions = connectActionSheet(CustomActions);
